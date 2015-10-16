eca6af025e51831bf98cf5955250fe6be72853c2
Luke Tomlin <ltomlin@palantir.com>
2015-10-16 17:57:52 +0100

    foo

--- crowdsourcer.py
+++ crowdsourcer.py
@@ -1,5 +1,5 @@
 import requests
-import grequests
+#import grequests
 from urllib2 import urlparse
 
 def ask_question(question, server, timeout=5):
@@ -11,14 +11,14 @@ def ask_question(question, server, timeout=5):
 def _netloc(url):
 	return urlparse.urlparse(url).netloc
 
-def distribute(question, participants, timeout=5):
-	scores = dict([(_netloc(p.url), int(p.score)) for p in participants])
-	rs = (grequests.get('%s?q=%s'%(p.url, question), timeout=timeout) for p in participants)
-	responses = grequests.map(rs)
-	answers = {}
-	for r in responses:
-		if r.ok:
-			answers[r.text] = answers.get(r.text, 0) + scores[_netloc(str(r.url))]
-	if answers:
-		return sorted(answers.items(), key=lambda x: x[1])[-1][0]
-	return None
+#def distribute(question, participants, timeout=5):
+#	scores = dict([(_netloc(p.url), int(p.score)) for p in participants])
+#	#rs = (grequests.get('%s?q=%s'%(p.url, question), timeout=timeout) for p in participants)
+	#responses = grequests.map(rs)
+##	answers = {}
+#	for r in responses:
+#		if r.ok:
+#			answers[r.text] = answers.get(r.text, 0) + scores[_netloc(str(r.url))]
+#	if answers:
+#		return sorted(answers.items(), key=lambda x: x[1])[-1][0]
+#	return None
