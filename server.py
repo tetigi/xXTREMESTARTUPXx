32cdd4cadd11adbe4b6c3a798d2c4fd0568b5fe7
Luke Tomlin <ltomlin@palantir.com>
2015-10-16 17:25:15 +0100

    Rigged server

--- server.py
+++ server.py
@@ -9,18 +9,20 @@ app = Flask(__name__)
 def answer():
     q = request.args.get("q", "")
 
-    print q
+    print "The question was: " + q
 
     leader_ans = None
 
     while not leader_ans:
         print 'Sleeping..'
-        time.sleep(5)
-        leader = "http://192.168.3.39:3000" # leaderboard.get_best_ip()
+        time.sleep(2)
+        # leader = "http://192.168.3.39:3000" # leaderboard.get_best_ip()
+        leader = "http://192.168.3.35:1337" # leaderboard.get_best_ip()
+        print "The leader is: " + str(leader)
         leader_ans = crowdsourcer.ask_question(q, leader)
-        print leader_ans
+        print "They replied with: " + str(leader_ans)
 
-    return "team_kickstarter" # leader_ans
+    return leader_ans
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9876, debug=True)
