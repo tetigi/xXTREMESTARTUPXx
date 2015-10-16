9e77648cd71aa8c7546f4998e962de8a61a17b94
Luke Tomlin <ltomlin@palantir.com>
2015-10-16 17:10:15 +0100

    Got initial layout for server

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
 from flask import Flask, request, jsonify
-import re
 
 app = Flask(__name__)
 
 @app.route("/")
 def answer():
     q = request.args.get("q", "")
+
     print q
-    return ":("
+
+    leader_ans = None
+
+    # while not leader_ans:
+        # leader = leaderboard.get_best_ip()
+        # leader_ans = crowdsourcer.ask_question(leader, q)
+
+    return ":(" # leader_ans
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9876, debug=True)
