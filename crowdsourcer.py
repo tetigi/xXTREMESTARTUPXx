9fae770953f7fba0ff546aab498dc77d81f1ad00
Chris Brockington-Hill <cbrockington@palantir.com>
2015-10-16 17:13:45 +0100

    crowdsourcer no http

--- crowdsourcer.py
+++ crowdsourcer.py
@@ -1,7 +1,7 @@
 import requests
 
-def ask_question(question, ip, timeout=5):
-	r = requests.get('http://%s?q=%s'%(ip, question), timeout)
+def ask_question(question, server, timeout=5):
+	r = requests.get('%s?q=%s'%(server, question), timeout)
 	if r.ok:
 		return r.text
 	return None
